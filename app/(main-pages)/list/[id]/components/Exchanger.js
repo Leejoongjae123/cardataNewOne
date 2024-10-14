54f286928d8b1eb811d4464623654cdbdd8a82b1
Leejoongjae123 <ljj3347@naver.com>
2024-10-14 18:15:17 +0900

    'exchange'

--- app/(main-pages)/list/[id]/components/Exchanger.js
+++ app/(main-pages)/list/[id]/components/Exchanger.js
@@ -5,6 +5,8 @@ import axios from 'axios'
 function Exchanger() {
   const [country1, setCountry1] = useState("KR");
   const [country2, setCountry2] = useState("US");
+  const [amount1,setAmount1]=useState(0)
+  const [amount2,setAmount2]=useState(0)
   const [amount, setAmount] = useState(0);
   const [direction, setDirection] = useState("down");
   const [result, setResult] = useState("");
@@ -18,23 +20,32 @@ function Exchanger() {
 
   const handleSubmit = async () => {
     try {
-      const response = await axios.post(
+      const response = await axios.get(
         "https://uapobh3i7k7x2724w5tkcium5m0xbfbt.lambda-url.ap-northeast-2.on.aws/exchange_rate",
         {
-          country1,
-          country2,
-          amount,
-          direction,
+          params: {
+            country1,
+            country2,
+            amount,
+            direction,
+          }
         }
       );
 
       // 응답 처리
       setResult(response.data);
+      if (direction==="down"){
+      setAmount2(response.data.country[1].value)
+      } else if (direction==="up"){
+        setAmount1(response.data.country[0].value)
+      }
+
     } catch (error) {
       console.error("Error:", error);
       
     } finally {
-      console.log('123')
+      console.log('11')
+      
     }
   };
   console.log("result:", result);
@@ -71,8 +82,10 @@ function Exchanger() {
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
             placeholder="Amount"
             required=""
+            value={amount1}
             onChange={(e) => {
               setAmount(e.target.value);
+              setAmount1(e.target.value)
               setDirection("down");
             }}
           />
@@ -99,8 +112,10 @@ function Exchanger() {
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
             placeholder=""
             required=""
+            value={amount2}
             onChange={(e) => {
               setAmount(e.target.value);
+              setAmount2(e.target.value)
               setDirection("up");
             }}
           />
