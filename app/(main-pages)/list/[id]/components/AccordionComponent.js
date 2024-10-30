0d04613eb980a4aa3d0919d142ea648b496a4923
Leejoongjae123 <ljj3347@naver.com>
2024-10-30 22:16:40 +0900

    '241030'

--- app/(main-pages)/list/[id]/components/AccordionComponent.js
+++ app/(main-pages)/list/[id]/components/AccordionComponent.js
@@ -6,14 +6,14 @@ export default function AccordionComponent({carData, dictionary, language }) {
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
 
   return (
-    <Accordion variant="splitted" className="my-5" defaultExpandedKeys={["1"]}>
+    <Accordion variant="splitted" className="my-5 dark:bg-white dark:text-black" defaultExpandedKeys={["1"]}>
       <AccordionItem
-        className="shadow-sm border border-gray-200"
+        className="shadow-sm border border-gray-200 dark:bg-white dark:text-black"
         key="1"
         aria-label={dictionary.detail.extint[language]}
         title={dictionary.detail.extint[language]}
       >
-        <ul className="my-5 grid grid-cols-3 space-y-4 text-gray-600 text-sm ">
+        <ul className="my-5 grid grid-cols-3 space-y-4 text-gray-600 dark:text-gray-600 text-sm ">
           {carData.optionExtInt.map((elem, index) => {
             return (
               <li className="flex items-center gap-3 col-span-1 text-center justify-center">
@@ -24,12 +24,12 @@ export default function AccordionComponent({carData, dictionary, language }) {
         </ul>
       </AccordionItem>
       <AccordionItem
-        className="shadow-sm border border-gray-200"
+        className="shadow-sm border border-gray-200 dark:bg-white dark:text-black"
         key="2"
         aria-label={dictionary.detail.safety[language]}
         title={dictionary.detail.safety[language]}
       >
-        <ul className="my-5 grid grid-cols-3 space-y-4 text-gray-600 text-sm ">
+        <ul className="my-5 grid grid-cols-3 space-y-4 text-gray-600 dark:text-gray-600 text-sm ">
           {carData.optionSafety.map((elem, index) => {
             return (
               <li className="flex items-center gap-3 col-span-1 text-center justify-center">
@@ -40,12 +40,12 @@ export default function AccordionComponent({carData, dictionary, language }) {
         </ul>
       </AccordionItem>
       <AccordionItem
-        className="shadow-sm border border-gray-200"
+        className="shadow-sm border border-gray-200 dark:bg-white dark:text-black"
         key="3"
         aria-label={dictionary.detail.convenience[language]}
         title={dictionary.detail.convenience[language]}
       >
-        <ul className="my-5 grid grid-cols-3 space-y-4 text-gray-600 text-sm ">
+        <ul className="my-5 grid grid-cols-3 space-y-4 text-gray-600 dark:text-gray-600 text-sm ">
           {carData.optionConvenience.map((elem, index) => {
             return (
               <li className="flex items-center gap-3 col-span-1 text-center justify-center">
@@ -56,12 +56,12 @@ export default function AccordionComponent({carData, dictionary, language }) {
         </ul>
       </AccordionItem>
       <AccordionItem
-        className="shadow-sm border border-gray-200"
+        className="shadow-sm border border-gray-200 dark:bg-white dark:text-black"
         key="4"
         aria-label={dictionary.detail.seat[language]}
         title={dictionary.detail.seat[language]}
       >
-        <ul className="my-5 grid grid-cols-3 space-y-4 text-gray-600 text-sm ">
+        <ul className="my-5 grid grid-cols-3 space-y-4 text-gray-600 dark:text-gray-600 text-sm ">
           {carData.optionSeat.map((elem, index) => {
             return (
               <li className="flex items-center gap-3 col-span-1 text-center justify-center">
@@ -72,12 +72,12 @@ export default function AccordionComponent({carData, dictionary, language }) {
         </ul>
       </AccordionItem>
       <AccordionItem
-        className="shadow-sm border border-gray-200"
+        className="shadow-sm border border-gray-200 dark:bg-white dark:text-black"
         key="5"
         aria-label={dictionary.detail.etc[language]}
         title={dictionary.detail.etc[language]}
       >
-        <ul className="my-5 grid grid-cols-3 space-y-4 text-gray-600 text-sm ">
+        <ul className="my-5 grid grid-cols-3 space-y-4 text-gray-600 dark:text-gray-600 text-sm ">
           {carData.optionEtc.map((elem, index) => {
             return (
               <li className="flex items-center gap-3 col-span-1 text-center justify-center">
